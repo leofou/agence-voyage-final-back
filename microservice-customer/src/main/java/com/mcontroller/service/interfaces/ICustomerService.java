21b5992098620856610e7696fc1d1ea3370659b9
joachim napias <joachim.napias@gmail.com>
2021-01-25 19:20:51 +0100

    Création du micro-service customer de base

---
+++ microservice-customer/src/main/java/com/mcontroller/service/interfaces/ICustomerService.java
@@ -0,0 +1,14 @@
+package com.mcontroller.service.interfaces;
+
+import java.util.List;
+
+import com.mcustomer.entities.Customer;
+
+public interface ICustomerService {
+	
+	List<Customer> findAll();
+	Customer findOne(Long id);
+	Customer save(Customer customer);
+	void delete(Long id);
+
+}
---
+++ microservice-customer/src/main/java/com/mcustomer/controller/CustomerController.java
@@ -0,0 +1,44 @@
+package com.mcustomer.controller;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mcontroller.service.interfaces.ICustomerService;
+import com.mcustomer.entities.Customer;
+
+@RestController
+public class CustomerController {
+	
+	@Autowired
+	ICustomerService iCustomerService;
+	
+	@GetMapping("/customers")
+	public List<Customer> findAll() {
+		return iCustomerService.findAll();
+	}
+
+	@GetMapping("/customers/{idCustomer}")
+	public Customer findOne(@PathParam("idCustomer") Long id) {
+		return iCustomerService.findOne(id);
+	}
+
+	@PostMapping("/customers")
+	public Customer save(@RequestBody Customer customer) {
+		return iCustomerService.save(customer);
+	}
+
+	@DeleteMapping("/customers/{idCustomer}")
+	public void delete(@PathParam("idCustomer")Long id) {
+		iCustomerService.delete(id);
+		
+	}
+
+}
