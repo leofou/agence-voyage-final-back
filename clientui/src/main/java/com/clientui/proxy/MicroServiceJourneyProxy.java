5f00a6e4745dd4bdff59b416cca5f221ba23605e
Alexis <you@example.com>
2021-01-27 10:17:01 +0100

    modif port mjourney proxy

--- clientui/src/main/java/com/clientui/proxy/MicroServiceJourneyProxy.java
+++ clientui/src/main/java/com/clientui/proxy/MicroServiceJourneyProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.clientui.beans.JourneyBean;
 
-@FeignClient(name="microservice-journey", url="localhost:9093/")
+@FeignClient(name="microservice-journey", url="localhost:9078/")
 public abstract interface MicroServiceJourneyProxy {
 	
 	@GetMapping(value="/journeys")
