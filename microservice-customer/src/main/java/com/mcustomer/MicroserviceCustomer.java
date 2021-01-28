3ea66597930c0b51f76a4835f30acea5b5bdaadb
joachim napias <joachim.napias@gmail.com>
2021-01-28 10:43:15 +0100

    modification du customer controller et ajout de la méthode update

--- microservice-customer/src/main/java/com/mcustomer/MicroserviceCustomer.java
+++ microservice-customer/src/main/java/com/mcustomer/MicroserviceCustomer.java
@@ -1,15 +1,8 @@
 package com.mcustomer;
 
-import java.util.stream.Stream;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-
-import com.mcustomer.entities.Customer;
-import com.mcustomer.repositories.CustomerRepository;
 
 @SpringBootApplication
 @EnableDiscoveryClient
