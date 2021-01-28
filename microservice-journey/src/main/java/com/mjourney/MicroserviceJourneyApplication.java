3e05a3fc904c92a699c0a871072da01d384b8bd8
Alexis <you@example.com>
2021-01-28 10:18:23 +0100

    corrections imports mjourney

--- microservice-journey/src/main/java/com/mjourney/MicroserviceJourneyApplication.java
+++ microservice-journey/src/main/java/com/mjourney/MicroserviceJourneyApplication.java
@@ -1,15 +1,8 @@
 package com.mjourney;
 
-import java.util.stream.Stream;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-
-import com.mjourney.entities.Journey;
-import com.mjourney.repositories.JourneyRepository;
 
 @SpringBootApplication
 @EnableDiscoveryClient
